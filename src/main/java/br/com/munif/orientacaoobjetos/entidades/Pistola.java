01135b62a4f223472e0b5513f32f6a4fd86bcd13
Munif Gebara Junior <munifgebara@gmail.com>
2016-03-07 21:30:42 -0300

    Atirando

---
+++ src/main/java/br/com/munif/orientacaoobjetos/entidades/Pistola.java
@@ -0,0 +1,13 @@
+package br.com.munif.orientacaoobjetos.entidades;
+
+public class Pistola extends Arma{
+
+    public Pistola() {
+        this.setDano(2);
+        this.setQuantidadeMunicao(100);
+    }
+    
+    
+    
+    
+}
